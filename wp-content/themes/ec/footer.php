d432c685e110840359f6637fb0ddcae9c869e671
Fabricio Zuardi <fabricio@fabricio.org>
2012-11-07 18:50:41 -0200

    removi clutter do footer

--- wp-content/themes/ec/footer.php
+++ wp-content/themes/ec/footer.php
@@ -14,9 +14,6 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'ec_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'ec' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'ec' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'ec' ), 'ec', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
