2ce9accb61d019ff5afb81a9ec3188f7cbc5ecd3
Fabricio Zuardi <fabricio@fabricio.org>
2012-11-08 19:12:05 -0200

    trabalho inicial para o layout da sessão pessoas

--- todo.md
+++ todo.md
@@ -6,3 +6,8 @@
 
 - lembrar de desligar o live plugin
 - limpar linhas comentadas no css
+
+-------------------------------------------------------------------------------
+# Detalhes:
+
+- arrumar o pequeno deslocamento de pixels no menu das internas entre o icone normal e o ativo
---
+++ wp-content/themes/ec/ec_pessoas.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Pessoas
+*/
+get_header();
+?>
+<div id="names_sidebar">
+  <ol>
+    <li>
+      <a href="">Valdir</a>
+    </li>
+    <li>
+      <a href="">Tiago</a>
+    </li>
+    <li>
+      <a href="">Fábio</a>
+    </li>
+  </ol>
+</div>
+<div id="bio">
+  <p>
+Designer por formação, Victor hoje é diretor <br>de criação na EC.
+  </p>
+  <p>
+Com mais de 15 anos de experiência Já <br>trabalhou na AlmapBBDO, Age, Giovanni+FCB.<br>Fez trabalhos para Nike, Kaiser, Fiat, Pepsi, Morumbi Shopping, e Folha de S. Paulo.
+  </p>
+  <p>
+Em sua formação: Basel Design School/Suíça, Arte e Tecnologia/PUC SP e Artes Gráficas pelo Senai SP.
+  </p>
+</div>
+<div class="clearfloat"></div>
+ <!-- <br style="clear:both;float:none;" /> -->
