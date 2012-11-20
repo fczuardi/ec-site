cda43fa53305dd6c492fef649bfc51e1b0cf6b3e
Armando Neto <neto.armando@gmail.com>
2012-11-20 20:36:25 -0200

    Pessoas: navegação (WIP)

--- wp-content/themes/ec/ec_pessoas.php
+++ wp-content/themes/ec/ec_pessoas.php
@@ -8,32 +8,32 @@ get_header();
   <ol>
     <li>
       <a href="#">VALDIR</a>
-      <p class="fullname">VICTOR ARAGÃO</p>
-      <p class="position">DIRETOR DE CRIAÇÃO</p>
-      <span class="email"><a href="mailto:victor@ec.vc">victor@ec.vc</a></span>
+      <p class="fullname">VALDIR WHATEVER</p>
+      <p class="position">DIRETOR<br/ >DE QUALQUER</p>
+      <span class="email"><a href="mailto:valdir@ec.vc">valdir@ec.vc</a></span>
     </li>
     <li>
       <a href="#">TIAGO</a>
-      <p class="fullname">VICTOR ARAGÃO</p>
-      <p class="position">DIRETOR DE CRIAÇÃO</p>
-      <span class="email"><a href="mailto:victor@ec.vc">victor@ec.vc</a></span>
+      <p class="fullname">TIAGO LARA</p>
+      <p class="position">DIRETOR<br/ >DE OPERAÇÕES</p>
+      <span class="email"><a href="mailto:tiago@ec.vc">tiago@ec.vc</a></span>
     </li>
     <li>
       <a href="#">FÁBIO</a>
-      <p class="fullname">VICTOR ARAGÃO</p>
-      <p class="position">DIRETOR DE CRIAÇÃO</p>
-      <span class="email"><a href="mailto:victor@ec.vc">victor@ec.vc</a></span>
+      <p class="fullname">FÁBIO JUNIOR</p>
+      <p class="position">DIRETOR<br/ >DE MÚSICA</p>
+      <span class="email"><a href="mailto:fabio@ec.vc">fabio@ec.vc</a></span>
     </li>
     <li>
       <a href="#">ANDERSON</a>
-      <p class="fullname">VICTOR ARAGÃO</p>
-      <p class="position">DIRETOR DE CRIAÇÃO</p>
-      <span class="email"><a href="mailto:victor@ec.vc">victor@ec.vc</a></span>
+      <p class="fullname">ANDERSON SILVA</p>
+      <p class="position">DIRETOR<br/ >DE MMA</p>
+      <span class="email"><a href="mailto:anderson@ec.vc">anderson@ec.vc</a></span>
     </li>
     <li class="active">
       <a href="#">VICTOR</a>
       <p class="fullname">VICTOR ARAGÃO</p>
-      <p class="position">DIRETOR DE CRIAÇÃO</p>
+      <p class="position">DIRETOR<br/ >DE CRIAÇÃO</p>
       <span class="email"><a href="mailto:victor@ec.vc">victor@ec.vc</a></span>
     </li>
   </ol>
@@ -51,6 +51,19 @@ Em sua formação: Basel Design School/Suíça, Arte e Tecnologia/PUC SP e Artes
 </div>
 <div class="clearfloat"></div>
  <!-- <br style="clear:both;float:none;" /> -->
+
+<script>
+  jQuery(document).ready(function ($) {
+
+    $('#names_sidebar li').click(function(index) {
+      $('#names_sidebar li').removeClass('active');
+      $(this).addClass('active');
+    });
+
+
+
+  });
+</script>
 
 <?php
 get_footer();
