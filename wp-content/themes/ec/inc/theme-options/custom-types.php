9dfa02f5e47ca458a68be6dbc5f997b848f3dba4
Armando Neto <neto.armando@gmail.com>
2012-11-20 10:48:44 -0200

    Custom type Pessoa: cargo, email

--- wp-content/themes/ec/inc/theme-options/custom-types.php
+++ wp-content/themes/ec/inc/theme-options/custom-types.php
@@ -38,26 +38,20 @@ add_action( 'add_meta_boxes', 'ec_type_pessoa_properties', 10, 2 );
 
 function ec_type_pessoa_properties() {
   add_meta_box(
-    'ec_pessoa_nome',
-    __( 'Nome', 'ec'),
-    'ec_admin_meta_pessoa_nome',
-    'ec_pessoa'
-  );
-  add_meta_box(
     'ec_pessoa_cargo',
     __( 'Cargo', 'ec'),
     'ec_admin_meta_pessoa_cargo',
     'ec_pessoa'
   );
+  add_meta_box(
+    'ec_pessoa_email',
+    __( 'Email', 'ec'),
+    'ec_admin_meta_pessoa_email',
+    'ec_pessoa'
+  );
 }
 
 
-function ec_admin_meta_pessoa_nome( $post ) {
-  $nome = get_post_meta($post->ID, '_nome', true);
-  echo '<table class="form-table">';
-  echo '<input type="text" name="_nome" placeholder="Seu nome" value="', $nome ? $nome : '','" size="30" style="width:75%; margin-right: 20px; float:left;" />';
-  echo '</table>';
-}
 function ec_admin_meta_pessoa_cargo( $post ) {
   $cargo = get_post_meta($post->ID, '_cargo', true);
   echo '<table class="form-table">';
@@ -65,6 +59,12 @@ function ec_admin_meta_pessoa_cargo( $post ) {
   echo '</table>';
 }
 
+function ec_admin_meta_pessoa_email( $post ) {
+  $email = get_post_meta($post->ID, '_email', true);
+  echo '<table class="form-table">';
+  echo '<input type="text" name="_email" placeholder="Seu email" value="', $email ? $email : '','" size="30" style="width:75%; margin-right: 20px; float:left;" />';
+  echo '</table>';
+}
 
 
 ?>
