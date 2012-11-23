e132aab11967b26ef6df960ac2b21d66e2f7e902
Fabricio Zuardi <fabricio@fabricio.org>
2012-11-23 18:39:33 -0200

    página de clientes

--- wp-content/themes/ec/ec_clientes.php
+++ wp-content/themes/ec/ec_clientes.php
@@ -5,17 +5,25 @@ Template Name: Clientes
 get_header();
 
 
-$clients = get_terms( 'cliente', 'orderby=count&hide_empty=0' );
-var_dump($clients);
+// $clients = get_terms( 'cliente', 'orderby=count&hide_empty=0' );
+$clients = get_terms( 'cliente', 'orderby=id&order=DESC&hide_empty=0' );
+?>
 
+<div class="grid">
+<?php
 foreach ($clients as $client) {
-  echo "<hr>";
   $image_src = s8_get_taxonomy_image_src($client, 'full');
-  var_dump($image_src);
-  // apply_filters('taxonomy_image_plugin_get_term_info', $client->term_id);
-  // var_dump(taxonomy_image_plugin_get_term_info(intval($client->term_id)));
-  // var_dump(apply_filters( 'taxonomy_image_plugin_get_term_info', $client['id']);
+  ?>
+      <div class="icon">
+        <img class="attachment-ec_trabalho_grid wp-post-image" src="<?php echo $image_src['src'];?>"/>
+      </div>
+  <?php
 }
+?>
+</div>
 
+<div class="clearfloat"></div>
+
+<?php
 get_footer();
 ?>
