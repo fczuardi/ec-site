865d3ed3752b31f99626e2cff6fd657cb9e143f8
Fabricio Zuardi <fabricio@fabricio.org>
2013-02-28 14:57:16 -0300

    do not display clients without image

--- wp-content/themes/ec/ec_clientes.php
+++ wp-content/themes/ec/ec_clientes.php
@@ -13,11 +13,13 @@ $clients = get_terms( 'cliente', 'orderby=slug&order=ASC&hide_empty=0' );
 <?php
 foreach ($clients as $client) {
   $image_src = s8_get_taxonomy_image_src($client, 'full');
+  if ($image_src){
   ?>
       <div class="icon">
         <img class="attachment-ec_trabalho_grid wp-post-image" src="<?php echo $image_src['src'];?>"/>
       </div>
   <?php
+  }
 }
 ?>
 </div>
